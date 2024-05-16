6e27ca2941b389c8c28d13b87a728764a35f2776
Thomas Hallström <minimoto@hotmail.com>
2024-05-16 23:35:37 +0200

    Add visible book item

--- Manero-Customer/Manero-Customer/Data/Models/ProductCategoryModel.cs
+++ Manero-Customer/Manero-Customer/Data/Models/ProductCategoryModel.cs
@@ -5,5 +5,7 @@
         public string? Title { get; set; }
         public string? Category { get; set; }
         public string? SubCategory { get; set; } 
+        public string? Author { get; set; }
+        public string? Price { get; set; }
     }
 }
---
+++ Manero-Customer/Manero-Customer/Data/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Manero_Customer.Data.Models
+{
+    public class ProductModel
+    {
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Price { get; set; } = null!;
+    }
+}
