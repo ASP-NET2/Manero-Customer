3f361904f28e77a181602a51d3e2ef9e41875cb4
Tina-Lutti <tina.lutti@yh.nackademin.se>
2024-05-25 12:36:39 +0200

    Updated design ProductDetails

--- Manero-Customer/Manero-Customer/Data/Models/ProductDetailsModel.cs
+++ Manero-Customer/Manero-Customer/Data/Models/ProductDetailsModel.cs
@@ -5,7 +5,10 @@
     public string Title { get; set; } = null!;
     public string Author { get; set; } = null!;
     public string Price { get; set; } = null!;
-    public string ImageUrl { get; set; } = null!;    
+    public string ImageUrl { get; set; } = null!;
+    public string ShortDescription { get; set; } = null!;
     public string LongDescription { get; set; } = null!;
+    public string FormatName { get; set; } = null!;
+   
    
 }
