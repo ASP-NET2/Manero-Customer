e1c114ec0a697c068b5968dcc6db280a162e2907
Thomas Hallström <minimoto@hotmail.com>
2024-05-28 15:44:41 +0200

    Adding products

---
+++ Manero-Customer/Manero-Customer/Data/Models/testModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Manero_Customer.Data.Models;
+
+public class TestModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Format { get; set; } = null!;
+    public int Price { get; set; }
+    public int DiscountPrice { get; set; }
+    public int Quantity { get; set; }
+    public string ImgUrl { get; set; } = null!;
+}
